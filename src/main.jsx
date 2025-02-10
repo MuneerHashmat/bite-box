f7236de821010f17093438087a4e38f44c3dbb68
Muneer Hashmat <muneerhashmat@gmail.com>
2025-02-10 21:44:39 +0530

    implemented lazy loading for all the routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import Index from "./Index.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux-store/store.js";
@@ -8,7 +8,7 @@ import { store } from "./redux-store/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Index />
     </Provider>
   </React.StrictMode>
 );
