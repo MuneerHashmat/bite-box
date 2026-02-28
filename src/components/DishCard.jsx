eb669ebeba6e6e602e168b9ff4f1117f39d534f5
Muneer Hashmat <muneerhashmat@gmail.com>
2026-02-28 17:33:51 +0530

    fix style of dish cards

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -46,53 +46,54 @@ const DishCard = ({ dish }) => {
     }
   };
   return (
-    <div className="rounded-md overflow-hidden cursor-pointer shadow-md relative">
-      <div className="overflow-hidden">
-        <img
-        src={dish.image}
-        alt={dish.name}
-        className="w-full object-cover hover:scale-110 transition-all duration-300 h-[256px]"
-      />
+    <div className="rounded-md overflow-hidden cursor-pointer shadow-md relative flex flex-col justify-between">
+      <div>
+        <div className="overflow-hidden">
+          <img
+            src={dish.image}
+            alt={dish.name}
+            className="w-full object-cover hover:scale-110 transition-all duration-300 h-[256px]"
+          />
+        </div>
+
+        <div className="px-4 py-2 mt-6 flex flex-col gap-2">
+          <h1 className=" font-semibold text-xl text-gray-700">{dish.name}</h1>
+          <div className="text-sm text-gray-500 flex gap-3">
+            <span>
+              <LocalShipping sx={{ fontSize: "16px", color: "orange" }} /> Free
+              Delivery
+            </span>
+            <span>
+              <Timer sx={{ fontSize: "16px", color: "green" }} /> 15 to 30 mins
+            </span>
+          </div>
+        </div>
       </div>
+      <div className="px-4 pb-4 mt-4 flex justify-between items-center">
+        <p className="text-gray-600">
+          Price: <span> &#8377;{dish.price}</span>
+        </p>
 
-      <div className="px-4 py-2 mt-6 flex flex-col gap-2">
-        <h1 className=" font-semibold text-xl text-gray-700">{dish.name}</h1>
-        <div className="text-sm text-gray-500 flex gap-3">
-          <span>
-            <LocalShipping sx={{ fontSize: "16px", color: "orange" }} /> Free
-            Delivery
-          </span>
-          <span>
-            <Timer sx={{ fontSize: "16px", color: "green" }} /> 15 to 30 mins
-          </span>
-        </div>
-
-        <div className="py-1 mt-4 flex justify-between items-center">
-          <p className="text-gray-600">
-            Price: <span> &#8377;{dish.price}</span>
-          </p>
-
-          {cartItem ? (
-            <div className="flex gap-2 px-2 py-1 bg-[#fc8019] text-white rounded-md">
-              <button onClick={handleIncrement}>
-                <AddCircleOutline />
-              </button>
-              <p className="w-[22px] text-center font-bold">
-                {cartItem.quantity}
-              </p>
-              <button onClick={handleDecrement}>
-                <RemoveCircleOutline />
-              </button>
-            </div>
-          ) : (
-            <button
-              onClick={handleAddToCart}
-              className="bg-[#FC8019] text-sm font-semibold text-white rounded-md p-2 hover:scale-105 transition-all"
-            >
-              Add to cart
+        {cartItem ? (
+          <div className="flex gap-2 px-2 py-1 bg-[#fc8019] text-white rounded-md">
+            <button onClick={handleIncrement}>
+              <AddCircleOutline />
             </button>
-          )}
-        </div>
+            <p className="w-[22px] text-center font-bold">
+              {cartItem.quantity}
+            </p>
+            <button onClick={handleDecrement}>
+              <RemoveCircleOutline />
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={handleAddToCart}
+            className="bg-[#FC8019] text-sm font-semibold text-white rounded-md p-2 hover:scale-105 transition-all"
+          >
+            Add to cart
+          </button>
+        )}
       </div>
 
       <div className="absolute right-[-12px] top-10 bg-white rounded-full pr-5 py-1">
