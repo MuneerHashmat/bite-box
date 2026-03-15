cff7d60030dff6f4da588a3d0adcae9d2c49f13b
Muneer Hashmat <muneerhashmat@gmail.com>
2026-03-15 14:30:44 +0530

    update gemini model

--- src/pages/ChatBot.jsx
+++ src/pages/ChatBot.jsx
@@ -23,7 +23,7 @@ const ChatBot = () => {
 
   const generteResponse = async (prompt) => {
     try {
-      const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+      const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
       const result = await model.generateContent(prompt);
       const response = result.response;
       const responseText = response.text();
